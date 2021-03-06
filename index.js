a59ea7eb56a75d84692920c51e0247fe82979c6b
nixigg86 <dev.py86@gmail.com>
2021-03-05 23:38:33 -0800

    working on button functionality, create boxes in the game

---
+++ index.js
@@ -0,0 +1,64 @@
+let $start = document.querySelector('#start');
+let $game = document.querySelector('#game');
+let $time = document.querySelector('#time');
+
+let score = 0;
+let isGameStarted = false
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
+
+function handleBoxClick(e) {
+  if(!isGameStarted) {
+    return 
+  }
+  if(e.target.dataset.box) {
+    score++
+    renderBox()
+  }
+}
+
+function startGame(){
+  isGameStarted = true
+  $start.classList.add('hide')
+  $game.style.backgroundColor = '#fff';
+
+  let interval = setInterval(function() {
+    let time = parseFloat($time.textContent)
+    if(time <= 0 ) {
+      clearInterval(interval)
+      endGame()
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1)
+    }
+  }, 100)
+
+  renderBox()
+}
+
+function endGame() {
+  isGameStarted = false
+}
+
+function renderBox() {
+  $game.innerHTML = '';
+  let box = document.createElement('div')
+  let boxSize = getRandom(30, 100)
+  let gameSize = $game.getBoundingClientRect()
+  let maxTop = gameSize.height - boxSize
+  let maxLeft = gameSize.width - boxSize
+
+  box.style.height = box.style.width = boxSize + 'px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = '#000';
+  box.style.top = getRandom(0, maxTop) + 'px';
+  box.style.left = getRandom(0, maxLeft) + 'px';
+  box.style.cursor = 'pointer';
+  box.setAttribute('data-box', 'true');
+
+  $game.insertAdjacentElement('afterbegin', box)
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
